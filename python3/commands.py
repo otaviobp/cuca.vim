c8fb2710dfb306d70cb28b56c54c8c2c138f95c4
Otavio Pontes <otaviobp@gmail.com>
2023-09-20 10:01:10 -0700

    commands: Add support to flags

--- python3/commands.py
+++ python3/commands.py
@@ -3,6 +3,10 @@ import errno
 import inspect
 
 
+def foo(flags):
+    return flags
+
+
 class CLICommander:
     def __init__(self, binary_name, cmds):
         self.commands = {
@@ -12,19 +16,46 @@ class CLICommander:
         self.commands.update(cmds)
         self.binary_name = binary_name
 
-    def __call(self, func, params):
+    def __call(self, func, params, flags):
         sig = inspect.signature(func)
+        kwargs = {}
+        for a in sig.parameters:
+            if a not in ["args", "flags"]:
+                if len(params) == 0:
+                    print("{}: Invalid number of parameters".format(self.binary_name))
+                    return errno.EINVAL
+                kwargs[a] = params.pop(0)
 
         if "args" in sig.parameters:
-            return func(args=params)
+            kwargs["args"] = params
+        else:
+            if len(params) > 0:
+                print("{}: Invalid number of parameters".format(self.binary_name))
+                return errno.EINVAL
 
-        if len(params) != len(sig.parameters):
-            print("{}: Invalid number of parameters".format(self.binary_name))
-            return errno.EINVAL
+        if "flags" in sig.parameters:
+            kwargs["flags"] = flags
 
-        return func(*params)
+        return func(**kwargs)
 
-    def run(self, args):
+    def __remove_flags(self, args):
+        new_args = []
+        flags = {}
+        for i in args:
+            if i.startswith("-"):
+                values = i.split("=", 2)
+                if len(values) < 2:
+                    values.append("")
+                flags[values[0]] = values[1]
+            else:
+                new_args.append(i)
+
+        return new_args, flags
+
+    def run(self, args, parse_flags=foo):
+        args, flags = self.__remove_flags(args)
+        flags = parse_flags(flags)
+
         i = 0
         commands = self.commands
         for i in range(len(args)):
@@ -33,7 +64,7 @@ class CLICommander:
                 break
 
             if callable(commands[cmd]):
-                return self.__call(commands[cmd], args[i + 1 :])
+                return self.__call(commands[cmd], args[i + 1 :], flags)
 
             if isinstance(commands[cmd], Mapping):
                 commands = commands[cmd]
@@ -42,7 +73,7 @@ class CLICommander:
                 return errno.EINVAL
 
         if "" in commands:
-            return self.__call(commands[""], args[i + 1 :])
+            return self.__call(commands[""], args[i + 1 :], flags)
 
         print(
             '{}: "{}" is not a valid {} command'.format(
